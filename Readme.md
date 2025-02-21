6eb8222c8068ff6c46c108170341225c2fdd4cf3
vk <v.kotteeswaran@uqu.do>
2025-02-21 17:18:33 +0530

    readme update

---
+++ Readme.md
@@ -0,0 +1,6 @@
+### PDF parsing with Python Unstructured ###
+
+```commandline
+chmod +x install_dependencies.sh
+python main.py
+```
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-apt install -y poppler-utils
-apt install -y tesseract-ocr
+sudo apt install -y poppler-utils
+sudo apt install -y tesseract-ocr
 pip install -r requirements.txt
 wget https://api.slingacademy.com/v1/sample-data/files/text-and-table.pdf #download sample pdf with text & tables
